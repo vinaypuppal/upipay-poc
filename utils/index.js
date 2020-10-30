ee2ee8029a3d53a93054fb06a13e23c8e84e2092
vínαч puppαl <8843216+vinaypuppal@users.noreply.github.com>
2020-10-30 19:49:37 +0000

    update UPI id

--- utils/index.js
+++ utils/index.js
@@ -2,13 +2,13 @@ import url from 'url';
 
 const URL = global.URL || url.URL;
 
-export function getURL({ amount, email }) {
+export function getURL({ amount }) {
   const url = new URL(`upi://pay`);
-  url.searchParams.append('pa', 'coderplex@yesbank');
-  url.searchParams.append('pn', 'Coderplex');
-  url.searchParams.append('currency', 'inr');
-  url.searchParams.append('tn', `Donation to coderplex from ${email}`);
+  url.searchParams.append('pa', '9666517697@ybl');
+  url.searchParams.append('pn', 'VinayPuppal');
+  url.searchParams.append('cu', 'INR');
+  // url.searchParams.append('tn', `Donation to coderplex from ${email}`);
   url.searchParams.append('am', amount);
-  url.searchParams.append('refUrl', `https://coderplex.org/donate`);
+  // url.searchParams.append('refUrl', `https://coderplex.org/donate`);
   return url.href;
 }
